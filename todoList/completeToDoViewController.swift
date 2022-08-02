d78849394abd57efa159f7bea53c770f742a8e10
zykx <zainayk2006@gmail.com>
2022-08-02 11:08:03 -0400

    Iteration 1 completed
    KWK iteration 2 completed (add todos functionality plus another view controller for marking things complete)

---
+++ todoList/completeToDoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  completeToDoViewController.swift
+//  todoList
+//
+//  Created by Zaina Khan on 8/2/22.
+//
+
+import UIKit
+
+class completeToDoViewController: UIViewController {
+    var previousVC = To_DoTableViewController()
+    var selectedToDo = ToDo()
+    @IBOutlet weak var titleLable: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLable.text = selectedToDo.name
+    }
+    
+    @IBAction func tappedComplete(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
